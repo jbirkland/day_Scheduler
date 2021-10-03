636df748be0239eedba00fa3c4a6aabee98af679
jbirkland <jbirk05@gmail.com>
2021-10-03 11:00:52 -0500

    comments added to js, js complete with adjustments

--- assets/script.js
+++ assets/script.js
@@ -1,75 +1,52 @@
 
-var allSaveButtons = $('.saveBtn')  // these are all the save buttons
-var textAreas = $('.textColumn');  // these are my text fields
-var hours = ["9am, 10am, 11am, 12am, 1pm, 2pm, 3pm, 4pm, 5pm"]   // these are my hour fields
-console.log (textAreas)
-console.log(hours)
-var hourIndex 
+var allSaveButtons = $('.saveBtn')  // these are all the save buttons linked as one
+var textAreas = $('.textColumn');  // these are my text fields to incorparate to local storage
 
 
-var toBeSaved = []                  // empty array for time and task to be saved
-var storageArr = JSON.parse(localStorage.getItem(" ")) || []    // from locStorage to page upon load
-
+//Function for retrieving scheduled item from local storage
 $('.row').each( function (){
     var textId = $(this).children('.textColumn').attr('id')
     var task = localStorage.getItem(textId);
     $ (this).children('.textColumn').val(task)
 })
 
+//Eventlistener for saving scheduled item to local storage
 allSaveButtons.on('click', function (event){
- // this is my event listener to put the time and text into local storage
-var textValue = $(this).siblings('.textColumn').val()
-var textId = $(this).siblings('.textColumn').attr('id')
-// storageArr.push(textValue)
-// localStorage.setItem('task', JSON.stringify(storageArr))
-localStorage.setItem(textId, textValue)
+    var textValue = $(this).siblings('.textColumn').val()
+    var textId = $(this).siblings('.textColumn').attr('id')
 
-    console.log (textId)
-            
-
+        localStorage.setItem(textId, textValue)
     
 })
 
-function getXandTxt(){
-    var UserToDo = textColumn;  // this function is to obtian the hour list(9am-5pm) as well as the text enter by user to be saved.
-    var hours =  hourColumn;
-    var scheduleObject = {
-        Time:hours,
-        Task:UserToDo
-    }
-    storageArr.push(schduleObject)
-    
-
-    console.log (scheduleObject)
-}
-
-
+//Function to retrieve time and date, link to page and other functions
 var todaysDate = new Date();
 var monthDay = { weekday: "long", month: "long", day: "numeric" };
 $("#currentDay").text(todaysDate.toLocaleDateString(undefined, monthDay));
-function changeColor() {
-    var timeTracker = new Date().getHours();
-         function pastElement(i, timeblockEl) {
+    function changeColor() {
+        var timeTracker = new Date().getHours();
+            function pastElement(i) {
             return i + 9 < timeTracker;
-         }
-        function presentElement(i, timeblockEl) {
+            }
+            function presentElement(i) {
             return i + 9 === timeTracker;
-         }
-        function futureElement(i, timeblockEl) {
+            }
+            function futureElement(i) {
             return i + 9 > timeTracker;
-        }
-        $(".time-block")
-            .filter(pastElement)
-            .addClass("past")
-            .removeClass("present future");
-        $(".time-block")
-            .filter(presentElement)
-            .addClass("present")
-            .removeClass("past future");
-        $(".time-block")
-            .filter(futureElement)
-            .addClass("future")
-            .removeClass("present past");
+            }
+
+            $(".time-block")
+                 .filter(pastElement)
+                .addClass("past")
+                .removeClass("present future");
+            $(".time-block")
+                .filter(presentElement)
+                .addClass("present")
+                .removeClass("past future");
+             $(".time-block")
+                .filter(futureElement)
+                .addClass("future")
+                .removeClass("present past");
 }
 changeColor();
 setInterval(changeColor, 1000);
